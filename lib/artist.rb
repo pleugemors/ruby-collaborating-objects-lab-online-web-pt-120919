b33ba1d49f6797321e88c31db3d57b4df6ac07cd
Paul Leugemors <pleugemors@yahoo.com>
2019-12-13 20:14:08 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -13,7 +13,6 @@ class Artist
   attr_accessor :name 
   
   def self.all
-    puts @@all
     @@all
   end
   
@@ -28,9 +27,8 @@ class Artist
   end
   
   def self.find_or_create_by_name(string_name)
-    #binding.pry
-    #artist = self.all.find{|art| art.name == string_name} 
-    #(artist.nil?) ? Artist.new(string_name) : artist 
+    artist = self.all.find{|art| art.name == string_name} 
+    (artist.nil?) ? Artist.new(string_name) : artist 
   end
   
   def print_songs
